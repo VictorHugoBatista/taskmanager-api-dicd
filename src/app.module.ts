90056fdb7b9bf9099e194cc4d9c9b819fed0e44a
Victor Hugo Batista <vct.bta@gmail.com>
2021-08-07 17:53:33 -0300

    feat: Changes the /healthcheck from the app controller to the common domain structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [CommonModule],
-  controllers: [AppController],
 })
 export class AppModule {}
--- src/common/controllers/healthcheck.controller.ts
+++ src/common/controllers/healthcheck.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('healthcheck')
-export class HealthcheckController {}
+export class HealthcheckController {
+  @Get()
+  @ApiOperation({ summary: 'Healthchecks the api' })
+  @ApiTags('Common')
+  healthCheck() {
+    return {
+      healthy: true,
+    };
+  }
+}
