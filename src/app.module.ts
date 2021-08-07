e06075eac1497a3721b2e15a605f7adb3fd6e218
Victor Hugo Batista <vct.bta@gmail.com>
2021-08-07 17:51:28 -0300

    feat: Add common domain basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule],
   controllers: [AppController],
 })
 export class AppModule {}
