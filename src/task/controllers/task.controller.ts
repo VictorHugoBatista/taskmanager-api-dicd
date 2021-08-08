d29c010d930b13e54fe549598fdd253a9dcea177
Victor Hugo Batista <vct.bta@gmail.com>
2021-08-08 11:03:16 -0300

    feat: Create the POST /task endpoint

--- src/task/controllers/task.controller.ts
+++ src/task/controllers/task.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  @Post()
+  @ApiOperation({ summary: 'Create task' })
+  @ApiTags('Tasks')
+  create() {
+    return {
+      healthy: true,
+    };
+  }
+}
