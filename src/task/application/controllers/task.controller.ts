ad89a50bd1105789464ea63a57d2ff98b754696c
Victor Hugo Batista <vct.bta@gmail.com>
2021-08-08 11:39:21 -0300

    feat: Map the POST /task parameters on the swagger

--- src/task/application/controllers/task.controller.ts
+++ src/task/application/controllers/task.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateTaskRequest } from '../../domain/requests/create-task-request';
 
 @Controller('task')
 export class TaskController {
   @Post()
   @ApiOperation({ summary: 'Create task' })
+  @ApiBody({ type: CreateTaskRequest })
   @ApiTags('Tasks')
   create(@Body() body: CreateTaskRequest) {
     // salva
