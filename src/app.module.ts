5f79843241d8c15ce4d24944eb3781b1856848ba
Victor Hugo Batista <vct.bta@gmail.com>
2021-08-08 11:00:50 -0300

    feat: Create the task domain base classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from './common/common.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [CommonModule, TypeOrmModule.forRoot()],
+  imports: [CommonModule, TaskModule, TypeOrmModule.forRoot()],
 })
 export class AppModule {}
