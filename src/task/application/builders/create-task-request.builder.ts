653a59da9eec1102c3310bf40f480b8d144a6561
Victor Hugo Batista <vct.bta@gmail.com>
2021-08-12 09:00:09 -0300

    feat: Generates the CreateTaskRequest object at the CreateTaskBuilder class

--- src/task/application/builders/create-task-request.builder.ts
+++ src/task/application/builders/create-task-request.builder.ts
@@ -1,10 +1,11 @@
+import { lorem } from 'faker';
 import { CreateTaskRequest } from '../contracts/requests/create-task-request';
 
 export class CreateTaskBuilder {
   public static build(): CreateTaskRequest {
     return {
-      title: 'test hardcoded t',
-      description: 'test hardcoded d',
+      title: lorem.sentence(),
+      description: lorem.sentences(),
     };
   }
 }
