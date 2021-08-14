e5dd6518c1974498f91d3cea37825981285bf2f7
Victor Hugo Batista <vct.bta@gmail.com>
2021-08-14 12:09:16 -0300

    feat: Add GET /task endpoint

--- src/task/application/controllers/task.controller.ts
+++ src/task/application/controllers/task.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateTaskRequest } from '../contracts/requests/create-task-request';
 import { TaskRepository } from '../../domain/repositories/task.repository';
@@ -8,6 +8,13 @@ import { Task } from '../contracts/entities/task.entity';
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskRepository: TaskRepository) {}
+
+  @Get()
+  @ApiOperation({ summary: 'List all the tasks' })
+  @ApiTags('Tasks')
+  public index() {
+    return this.taskRepository.list();
+  }
 
   @Post()
   @ApiOperation({ summary: 'Create task' })
