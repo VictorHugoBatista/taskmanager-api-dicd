f27a832450dabceb2915161037a1fe6529081dc5
Victor Hugo Batista <vct.bta@gmail.com>
2021-08-19 19:30:49 -0300

    feat: Add e2e tests for the task update new endpoint

---
+++ test/task/update.e2e-spec.ts
@@ -0,0 +1,81 @@
+import { INestApplication } from '@nestjs/common';
+import { lorem } from 'faker';
+import * as request from 'supertest';
+import { MongodbHelper } from '../../src/common/application/helpers/mongodb';
+import { CreateTaskBuilder } from '../../src/task/application/builders/create-task-entity.builder';
+import { InMemoryTaskRepository } from '../../src/task/application/repositories/in-memory-task-repository';
+import { TaskModuleTest } from './builders/task-module-test';
+
+describe('TaskController.delete (e2e)', () => {
+  let app: INestApplication;
+  let repository: InMemoryTaskRepository;
+
+  beforeEach(async () => {
+    await TaskModuleTest.startModule();
+    app = TaskModuleTest.app;
+    repository = TaskModuleTest.repository;
+    repository.data = [
+      CreateTaskBuilder.build(),
+      CreateTaskBuilder.build(),
+      CreateTaskBuilder.build(),
+    ];
+  });
+
+  it('should update title and return the given task id', () => {
+    const taskForSearch = repository.data[2];
+    taskForSearch.title = lorem.sentence();
+    return request(app.getHttpServer())
+      .patch(`/task/${taskForSearch.id}`)
+      .send(taskForSearch)
+      .expect(200)
+      .expect(({ body }) => {
+        expect(JSON.stringify(body)).toEqual(JSON.stringify(taskForSearch));
+        expect(JSON.stringify(repository.data[2])).toEqual(
+          JSON.stringify(taskForSearch),
+        );
+      });
+  });
+
+  it('should update description and return the given task id', () => {
+    const taskForSearch = repository.data[2];
+    taskForSearch.description = lorem.sentences();
+    return request(app.getHttpServer())
+      .patch(`/task/${taskForSearch.id}`)
+      .send(taskForSearch)
+      .expect(200)
+      .expect(({ body }) => {
+        expect(JSON.stringify(body)).toEqual(JSON.stringify(taskForSearch));
+        expect(JSON.stringify(repository.data[2])).toEqual(
+          JSON.stringify(taskForSearch),
+        );
+      });
+  });
+
+  it('should update title and description and return the given task id', () => {
+    const taskForSearch = repository.data[2];
+    taskForSearch.title = lorem.sentence();
+    taskForSearch.description = lorem.sentences();
+    return request(app.getHttpServer())
+      .patch(`/task/${taskForSearch.id}`)
+      .send(taskForSearch)
+      .expect(200)
+      .expect(({ body }) => {
+        expect(JSON.stringify(body)).toEqual(JSON.stringify(taskForSearch));
+        expect(JSON.stringify(repository.data[2])).toEqual(
+          JSON.stringify(taskForSearch),
+        );
+      });
+  });
+
+  it('should return 404 for non existing task', () => {
+    return request(app.getHttpServer())
+      .patch(`/task/${MongodbHelper.generateObjectId()}`)
+      .expect(404);
+  });
+
+  it('should return 404 for invalid task id', () => {
+    return request(app.getHttpServer())
+      .patch('/task/nonn-valid-task-id')
+      .expect(404);
+  });
+});
