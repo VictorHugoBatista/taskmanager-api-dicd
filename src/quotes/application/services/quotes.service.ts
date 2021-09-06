ff708eb24cebc33e24ddcd4eed75b3841ab7859a
Victor Hugo Batista <vct.bta@gmail.com>
2021-09-06 16:10:14 -0300

    feat: Call the random-data-api from the random quotes new endpoint

--- src/quotes/application/services/quotes.service.ts
+++ src/quotes/application/services/quotes.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class QuotesService {
+  private baseUrl: string = 'https://random-data-api.com/api';
+
+  constructor(private httpService: HttpService) {}
+
   public random() {
-    return {quote: 'quote'};
+    const quote = this.httpService.get(
+      `${this.baseUrl}/lorem_ipsum/random_lorem_ipsum`,
+    );
+    return quote;
   }
 }
--- src/quotes/quotes.module.ts
+++ src/quotes/quotes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { QuotesController } from './application/controllers/quotes.controller';
 import { QuotesService } from './application/services/quotes.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [QuotesController],
   providers: [QuotesService],
 })
